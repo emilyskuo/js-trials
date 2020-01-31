74f7a0026f339558c32ab72c601842f5c1b0e29f
Your Name <you@example.com>
2020-01-30 16:41:33 -0800

    functions.js skills functions implemented in js

--- functions.js
+++ functions.js
@@ -2,15 +2,45 @@
 
 
 // 1. isHometown
+function isHometown(town) {
+  const homeTown = 'Saratoga';
+  if (homeTown == town){
+    return true;
+  }
+  else {
+    return false;
+  }
 
-// Define your function here
+
+}
 
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
 
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax = 0.05) {
+  let subtotal = basePrice * (1 + tax);
+  let fee = 0;
+  if (state == 'CA') {
+    fee = 0.03 * subtotal;
+  } // end CA if
+  else if (state == 'PA') {
+    fee = 2;
+  } // end PA if
+  else if (state == 'MA') {
+    if (basePrice <= 100) {
+      fee = 1;
+    } // end MA < 100 if
+    else {
+      fee = 3;
+    } // end MA else
+  } // end MA 
+
+  return subtotal + fee;
+} // end function
